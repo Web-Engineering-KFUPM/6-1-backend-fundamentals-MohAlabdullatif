7ca2f212f70a858babe77d124e08bc7f1ff69888
MohForSupport <bykelver1@gmail.com>
2026-03-30 10:26:48 +0300

    Intro to the backend

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,25 +1,34 @@
 // TODO 1: Import Express
+import express from 'express';
 
+// TODO 2: Allow React to access the server
+import cors from 'cors';
 
+// TODO 3: Create the Express app
+const app = express();
 
-// TODO 2: Create the Express app and store it in a variable named app
+// enable CORS
+app.use(cors());
 
+// TODO 4: Create the home route "/"
+app.get('/', (req, res) => {
+  res.send('Hello from the back-end server');
+});
 
+// TODO 5: Create the "/about" route
+app.get('/about', (req, res) => {
+  res.send('This is the about route');
+});
 
-// TODO 3: Allow React to access the server
+// TODO 6: Create the "/student" route
+app.get('/student', (req, res) => {
+  res.json({
+    name: 'Aisha',
+    major: 'Computer Science'
+  });
+});
 
-
-
-// TODO 5: Create the home route "/"
-
-
-
-// TODO 6: Create the "/about" route
-
-
-
-// TODO 7: Create the "/student" route
-
-
-
-// TODO 4: Start the server on port 3000
+// TODO 7: Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -351,24 +351,26 @@ After you finish, answer these questions:
 END OF LAB INSTRUCTIONS
 ===================================================================
 */
-
 import { useEffect, useState } from 'react';
 import './index.css';
 
 export default function App() {
   // TODO 9:
   // Create state to store student data
-  // Syntax hint:
-  // const [student, setStudent] = useState(____);
+  const [student, setStudent] = useState(null);
 
   // TODO 8:
   // Request student data from the server when the page loads
-  // Syntax hint:
-  // fetch('http://localhost:3000/student')
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     setStudent(data);
-  //   });
+  useEffect(() => {
+    fetch('http://localhost:3000/student')
+      .then((res) => res.json())
+      .then((data) => {
+        setStudent(data);
+      })
+      .catch((err) => {
+        console.error('Error fetching student:', err);
+      });
+  }, []);
 
   return (
     <main className="app-shell">
